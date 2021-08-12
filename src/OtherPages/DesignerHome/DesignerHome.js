0c132076b5852441bb0bf41a9d7a667e49f3f327
mukesh kushwah <m.kushwah33@gmail.com>
2021-08-12 22:26:48 +0530

    Update DesignerHome.js
    changes

--- src/OtherPages/DesignerHome/DesignerHome.js
+++ src/OtherPages/DesignerHome/DesignerHome.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios from '../../utils//axios.config.js';
 
 import DesignerWear from "./Components/DesignerWear";
 import TopCategory from "./Components/TopCategory";

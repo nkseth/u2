0668fedbd30d6b43d120b67cebdbdcc280b1fb50
Om Kumar <omkumar@Oms-MacBook-Air.local>
2021-08-28 17:16:48 +0530

    new changes

--- src/LoginSceens/Login.js
+++ src/LoginSceens/Login.js
@@ -1,16 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./style/Login.module.scss";
 import { Link } from "react-router-dom";
 import { Button } from "@material-ui/core";
 import GoogleIcon from "../Images/login/google.svg";
-import {useMediaQuery} from '@material-ui/core'
+import { useMediaQuery } from '@material-ui/core'
 import FacebookIcon from "../Images/login/facebook.svg";
 import { useTheme } from '@material-ui/core/styles';
 import useLogin from "./useLogin";
+import common_axios from "../utils/axios.config";
+import { useSelector, useDispatch } from "react-redux";
+import { setLoginCreds } from "../Redux/actions/homepage";
+
+
 const Login = () => {
+
   const { login_Mode_Handler, loginMode } = useLogin();
-  const theme=useTheme()
-  const match=useMediaQuery(theme.breakpoints.down('sm'))
+  const dispatch = useDispatch()
+  const theme = useTheme()
+  const match = useMediaQuery(theme.breakpoints.down('sm'));
+  const { login_creds } = useSelector( state => state.root.login)
+
+  const { email, password } = login_creds;
+
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+
+  const login_handler = async () => {
+
+
+    if (email.length == 0) {
+      alert("Enter a valid email")
+      setError({ type: 'email', value: "Enter a valid email" })
+      return;
+    }
+
+    if (password.length < 6) {
+      alert("Enter a valid password")
+      setError({ type: 'password', value: "Enter a valid password" })
+      return;
+    }
+
+    setLoading(true)
+
+    try {
+      const { data } = await common_axios.post('/auth/login', {
+        email,
+        password
+      })
+
+      console.log(data)
+
+      if (data) {
+        if (data.success) {
+          login_Mode_Handler("LoginOtpVarify");
+        }
+      }
+    } catch (e) {
+      alert('Invalid Email or Password')
+      console.log(e)
+    }
+  }
+
+
   return (
     <form onSubmit={(e) => e.preventDefault()} className={styles.Screen_Login}>
       <div className={styles.Screen_Login_Link}>
@@ -33,11 +84,11 @@ const Login = () => {
       </div>
       <div className={styles.Screen_Login_Input}>
         <label>Email Id/Phone Number</label>
-        <input type="text" placeholder="Email Id/Phone Number" />
+        <input value={email} onChange={(e) => dispatch(setLoginCreds({...login_creds, email:e.target.value}))} type="text" placeholder="Email Id/Phone Number" />
       </div>
       <div className={styles.Screen_Login_Input}>
         <label>Password</label>
-        <input type="password" placeholder="password" />
+        <input value={password} onChange={(e) => dispatch(setLoginCreds({...login_creds, password:e.target.value}))} type="password" placeholder="password" />
         <span>
           <Link onClick={() => login_Mode_Handler("ForgotPasswordOtp")}>
             Forgot password
@@ -45,7 +96,7 @@ const Login = () => {
         </span>
       </div>
       <div className={styles.Screen_Login_Button}>
-        <Button onClick={() => login_Mode_Handler("LoginOtpVarify")}>
+        <Button onClick={() => login_handler()}>
           Login
         </Button>
       </div>
@@ -53,11 +104,11 @@ const Login = () => {
         <span>OR</span>
       </div>
       {
-        match?null:<div className={styles.Screen_Login_Button}>
-        <Button>Login with Mobile Number</Button>
-      </div>
+        match ? null : <div className={styles.Screen_Login_Button}>
+          <Button>Login with Mobile Number</Button>
+        </div>
       }
-      
+
       <div className={styles.Screen_Login_Social}>
         <Button>
           <img src={GoogleIcon} alt="GoogleIcon" />
--- src/Redux/reducers/rootreducer.js
+++ src/Redux/reducers/rootreducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import loginreducer from "./loginreducer";
 import homepageReducer from "./homepagereducer";
+import mainreducer from "./mainReducer";
 
 const rootreducer = combineReducers({
   login: loginreducer,
   homepage: homepageReducer,
+  main: mainreducer,
 });
 
 export default rootreducer;

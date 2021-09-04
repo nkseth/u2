3f79d0fd117162cb1f0d016ed58976e8a79a5c53
Om Kumar <omkumar@Oms-MacBook-Air.local>
2021-09-04 15:59:02 +0530

    Changed IP

--- src/utils/axios.config.js
+++ src/utils/axios.config.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
  const common_axios = Axios.create({
-  baseURL: "http://3.108.240.44/api",
+  baseURL: "http://3.109.176.19/api",
 });
 
 common_axios.interceptors.request.use(

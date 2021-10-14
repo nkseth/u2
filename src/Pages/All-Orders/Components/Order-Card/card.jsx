f39a3d57fa8df0ab239ba8e05978729cd11e13f7
Yogesh <yogesh.kumar.rr5@gmail.com>
2021-10-14 18:46:35 +0530

    Added new Order Pages

--- src/Pages/All-Orders/Components/Order-Card/card.jsx
+++ src/Pages/All-Orders/Components/Order-Card/card.jsx
@@ -1,39 +1,36 @@
-import React from "react";
-import { Button, useMediaQuery } from "@material-ui/core";
-import { Link, useHistory } from "react-router-dom";
-import CustomDivider from "../../../../utils/Custom Divider/divider";
-import styles from "./card.module.scss";
+import React from 'react';
+import { Button, useMediaQuery } from '@material-ui/core';
+import { Link, useHistory } from 'react-router-dom';
+import CustomDivider from '../../../../utils/Custom Divider/divider';
+import styles from './card.module.scss';
 
-export default function OrdersCard({ item, orderId }) {
-  // console.log(item);
+export default function OrdersCard({ item, orderId, detail }) {
+  console.log('🚀 ~ file: card.jsx ~ line 8 ~ OrdersCard ~ item', item);
+
   const history = useHistory();
-  const mobileView = useMediaQuery("(max-width:550px)");
+  const mobileView = useMediaQuery('(max-width:550px)');
   return (
     <div className={styles.mainContainer}>
-      <CustomDivider />
+      {!detail && <CustomDivider />}
       <div className={styles.cardContainer}>
         <img
           src={item.product.image}
           alt={item.title}
-          style={{ cursor: "pointer" }}
+          style={{ cursor: 'pointer' }}
           onClick={() => history.push(`/product-description/${item.slug}`)}
         />
         <div className={styles.detailsDiv}>
-          <div className={styles.detailTwo}>
-            <span>Quantity:</span>
-            <span>{item.quantity}</span>
-          </div>
           <div className={styles.detailOne}>
             <div>
-              <span
-                style={{ cursor: "pointer" }}
+              <h1
+                style={{ cursor: 'pointer' }}
                 onClick={() =>
                   history.push(`/product-description/${item.slug}`)
                 }
               >
                 {item.title}
-              </span>
-              {/* <span>Solid Straight Kurta</span> */}
+              </h1>
+              <span>Solid Straight Kurta</span>
             </div>
 
             <div>
@@ -41,13 +38,17 @@ export default function OrdersCard({ item, orderId }) {
               {item.total}
             </div>
           </div>
+          <div className={styles.detailTwo}>
+            <span>Quantity:</span>
+            <span>{item.quantity}</span>
+          </div>
           {mobileView && (
             <div
               style={{
-                fontFamily: "DM Serif Display",
-                fontSize: "28px",
+                fontFamily: 'DM Serif Display',
+                fontSize: '28px',
                 fontWeight: 400,
-                lineHeight: "36px",
+                lineHeight: '36px',
               }}
             >
               {item.currency_symbol}
@@ -58,17 +59,18 @@ export default function OrdersCard({ item, orderId }) {
           <Button
             onClick={() => history.push(`/trackorder/${orderId}`)}
             className={styles.trackBtn}
-            variant="contained"
+            variant='contained'
           >
             Track Order
           </Button>
           <div className={styles.detailThree}>
-            <span
-              style={{ cursor: "pointer" }}
+            <Link
+              to={`/order-details/${orderId}`}
+              style={{ cursor: 'pointer' }}
               // onClick={() => history.push(`/rate_order/${orderId}`)}
             >
               Order Detail
-            </span>
+            </Link>
             <span></span>
             {/* <Link>Cancel Order</Link> */}
           </div>
--- src/Pages/All-Orders/allOrders.jsx
+++ src/Pages/All-Orders/allOrders.jsx
@@ -1,42 +1,43 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
-import { Grid, useMediaQuery } from "@material-ui/core";
-import Container from "../../utils/Container/container";
-import CustomDivider from "../../utils/Custom Divider/divider";
-import SideNavbar from "../../utils/Side-Navbar/sideNavbar";
-import Breadcrumb from "../../utils/Breadcrumb/breadcrumb";
-import OrdersCard from "./Components/Order-Card/card";
-import styles from "./allOrders.module.scss";
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Grid, useMediaQuery } from '@material-ui/core';
+import Container from '../../utils/Container/container';
+import CustomDivider from '../../utils/Custom Divider/divider';
+import SideNavbar from '../../utils/Side-Navbar/sideNavbar';
+import Breadcrumb from '../../utils/Breadcrumb/breadcrumb';
+import OrdersCard from './Components/Order-Card/card';
+import styles from './allOrders.module.scss';
 //icons
-import DeliveryVanIcon from "../../Images/icons/devliveryVan.svg";
-import PackageIcon from "../../Images/icons/package.svg";
-import PastOrdersCard from "./Components/Past-orders/card";
-import { useDispatch, useSelector } from "react-redux";
-import { get_orders } from "../../Redux/actions/profile";
-import Loader from "../../utils/Loader/Loader";
+import DeliveryVanIcon from '../../Images/icons/devliveryVan.svg';
+import PackageIcon from '../../Images/icons/package.svg';
+import PastOrdersCard from './Components/Past-orders/card';
+import { useDispatch, useSelector } from 'react-redux';
+import { get_orders } from '../../Redux/actions/profile';
+import Loader from '../../utils/Loader/Loader';
 
 export default function AllOrders() {
   const dispatch = useDispatch();
-  const { orders } = useSelector((state) => state.root.profile);
+  const { orders } = useSelector(state => state.root.profile);
   console.log(orders);
   useEffect(() => {
     dispatch(get_orders());
   }, [dispatch]);
-  const tabView = useMediaQuery("(max-width:768px)");
-  const mobileView = useMediaQuery("(max-width:550px)");
+  const tabView = useMediaQuery('(max-width:768px)');
+  const mobileView = useMediaQuery('(max-width:550px)');
   let pendingOrders = [],
     confirmedOrders = [],
     deliverdOrders = [];
+
   if (orders) {
-    orders.forEach((order) => {
+    orders.forEach(order => {
       if (
-        order.order_status.toLowerCase() === "pending" ||
-        order.order_status.toLowerCase() === "pending"
+        order.order_status.toLowerCase() === 'pending' ||
+        order.order_status.toLowerCase() === 'pending'
       )
         pendingOrders.push(order);
-      if (order.order_status.toLowerCase() === "confirmed")
+      if (order.order_status.toLowerCase() === 'confirmed')
         confirmedOrders.push(order);
-      if (order.order_status.toLowerCase() === "delivered")
+      if (order.order_status.toLowerCase() === 'delivered')
         deliverdOrders.push(order);
     });
 
@@ -47,7 +48,7 @@ export default function AllOrders() {
     // );
   }
   return (
-    <Container bottomDivider pBottom="0" footerOnTabMob>
+    <Container bottomDivider pBottom='0' footerOnTabMob>
       {!orders ? (
         <Loader />
       ) : (
@@ -55,7 +56,7 @@ export default function AllOrders() {
           {!tabView && <SideNavbar />}
           <div className={styles.cardsDivOuterContainer}>
             {tabView && (
-              <Breadcrumb path="Home /" activePath="Profile / Orders" />
+              <Breadcrumb path='Home /' activePath='Profile / Orders' />
             )}
             <div className={styles.headerDiv}>
               <span className={styles.header}>Orders</span>
@@ -65,44 +66,46 @@ export default function AllOrders() {
               <div>
                 <div
                   style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "center",
-                    gap: "1rem",
-                    padding: "1rem 0",
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    gap: '1rem',
+                    padding: '1rem 0',
                   }}
                 >
-                  <img src={DeliveryVanIcon} alt="Delivery Van" />
-                  {"On the way"}
+                  <img src={DeliveryVanIcon} alt='Delivery Van' />
+                  {'On the way'}
                 </div>
                 <CustomDivider />
                 {confirmedOrders.map(
-                  ({ id, customer, items, order_number }) => (
-                    <div className={styles.ordersCardDiv}>
-                      {/* {console.log(order)} */}
-                      {items.map((item) => (
-                        <OrdersCard item={item} key={item.id} orderId={id} />
-                      ))}
+                  ({ id, customer, items, order_number }) => {
+                    return (
+                      <div className={styles.ordersCardDiv}>
+                        {/* {console.log(order)} */}
+                        {items.map(item => (
+                          <OrdersCard item={item} key={item.id} orderId={id} />
+                        ))}
 
-                      <div className={styles.deliveryAddress}>
-                        <span>Delivery Address</span>
-                        <p>
-                          <span>{customer.name}</span>
-                          <span>{customer.phone_no}</span>
-                        </p>
-                        <p>
-                          No 167, 2nd floor, 3rd cross RK garden behind gowri
-                          appatment mathikere bengaluru, Mathikere, Bengaluru -
-                          560054
-                        </p>
-                        <p>
-                          <span>Order ID</span>
-                          <span>{order_number}</span>
-                        </p>
+                        {/* <div className={styles.deliveryAddress}>
+                          <span>Delivery Address</span>
+                          <p>
+                            <span>{customer.name}</span>
+                            <span>{customer.phone_no}</span>
+                          </p>
+                          <p>
+                            No 167, 2nd floor, 3rd cross RK garden behind gowri
+                            appatment mathikere bengaluru, Mathikere, Bengaluru
+                            - 560054
+                          </p>
+                          <p>
+                            <span>Order ID</span>
+                            <span>{order_number}</span>
+                          </p>
+                        </div> */}
+                        <CustomDivider />
                       </div>
-                      <CustomDivider />
-                    </div>
-                  )
+                    );
+                  }
                 )}
               </div>
             )}
@@ -110,21 +113,21 @@ export default function AllOrders() {
               <div>
                 <div
                   style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "center",
-                    gap: "1rem",
-                    padding: "1rem 0",
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    gap: '1rem',
+                    padding: '1rem 0',
                   }}
                 >
-                  <img src={DeliveryVanIcon} alt="Delivery Van" />
-                  {"Pending"}
+                  <img src={DeliveryVanIcon} alt='Delivery Van' />
+                  {'Pending'}
                 </div>
                 <CustomDivider />
                 {pendingOrders.map(({ id, customer, items, order_number }) => (
                   <div className={styles.ordersCardDiv}>
                     {/* {console.log(order)} */}
-                    {items.map((item) => (
+                    {items.map(item => (
                       <OrdersCard item={item} key={item.id} orderId={id} />
                     ))}
 
@@ -154,20 +157,20 @@ export default function AllOrders() {
               <div>
                 <div
                   style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "center",
-                    gap: "1rem",
-                    padding: "1rem 0",
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    gap: '1rem',
+                    padding: '1rem 0',
                   }}
                 >
-                  <img src={PackageIcon} alt="orders" />
-                  {"Past Orders"}
+                  <img src={PackageIcon} alt='orders' />
+                  {'Past Orders'}
                 </div>
                 <CustomDivider />
                 {deliverdOrders.map(({ id, customer, items, order_number }) => (
                   <div className={styles.ordersCardDiv}>
-                    {items.map((item) => (
+                    {items.map(item => (
                       <PastOrdersCard item={item} key={item.id} orderId={id} />
                     ))}
 

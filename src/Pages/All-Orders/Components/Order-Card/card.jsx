e6d61582a503c68738e9b10fe225ad9b0faa57a6
iamvishalchandra <vishalchandra.75@gmail.com>
2021-10-17 18:57:11 +0530

    fixed ui issues on order detail

--- src/Pages/All-Orders/Components/Order-Card/card.jsx
+++ src/Pages/All-Orders/Components/Order-Card/card.jsx
@@ -31,7 +31,7 @@ export default function OrdersCard({ pending, item, orderId }) {
               <span>Solid Straight Kurta</span>
             </div>
 
-            <div>
+            <div style={{ fontWeight: "bold", fontSize: "1.2rem" }}>
               {item.currency_symbol}
               {item.total}
             </div>

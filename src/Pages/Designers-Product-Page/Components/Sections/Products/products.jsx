b6eb570421e5a4d2e81ffbe806a450b1fc37a84e
Yogesh <yogesh.kumar.rr5@gmail.com>
2021-10-18 19:28:24 +0530

    Added Skeleton Loader

--- src/Pages/Designers-Product-Page/Components/Sections/Products/products.jsx
+++ src/Pages/Designers-Product-Page/Components/Sections/Products/products.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import {
   Grid,
   FormControl,
@@ -9,32 +9,32 @@ import {
   ButtonGroup,
   Button,
   Drawer,
-} from "@material-ui/core";
-import Loader from "../../../../../utils/Loader/Loader";
-import cx from "classnames";
-import ProductCard from "../../product-card/card";
-import Filter from "../Filter/filter";
-import styles from "./product.module.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { getWishList } from "../../../../../Redux/actions/wishlist";
+} from '@material-ui/core';
+import Loader, { ProductLoader } from '../../../../../utils/Loader/Loader';
+import cx from 'classnames';
+import ProductCard from '../../product-card/card';
+import Filter from '../Filter/filter';
+import styles from './product.module.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { getWishList } from '../../../../../Redux/actions/wishlist';
 export default function ProductsSection({ products, loading }) {
   const dispatch = useDispatch();
-  const tabViewPro = useMediaQuery("(max-width:835px)");
-  const tabView = useMediaQuery("(max-width:550px)");
-  const mobileView = useMediaQuery("(max-width:550px)");
-  const [sortBy, setSortBy] = useState("");
+  const tabViewPro = useMediaQuery('(max-width:835px)');
+  const tabView = useMediaQuery('(max-width:550px)');
+  const mobileView = useMediaQuery('(max-width:550px)');
+  const [sortBy, setSortBy] = useState('');
   const [isFilterOpen, setFilterOpen] = useState(false);
 
-  const { user, isAuthenticated } = useSelector((state) => state.root.auth);
+  const { user, isAuthenticated } = useSelector(state => state.root.auth);
   const [temp, setTemp] = useState();
 
-  const handleSort = (e) => {
+  const handleSort = e => {
     setSortBy(e.target.value);
-    if (e.target.value === "lowToHigh") {
+    if (e.target.value === 'lowToHigh') {
       setTemp(products.sort((a, b) => a.price - b.price));
     }
 
-    if (e.target.value === "highToLow") {
+    if (e.target.value === 'highToLow') {
       setTemp(
         products.sort((a, b) => {
           if (b.custom_price >= 1) {
@@ -59,11 +59,11 @@ export default function ProductsSection({ products, loading }) {
   //   // setProducts(temp);
   // }, [sortBy]);
 
-  const toggleDrawer = (anchor, open) => (event) => {
+  const toggleDrawer = (anchor, open) => event => {
     if (
       event &&
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
     ) {
       return;
     }
@@ -75,14 +75,16 @@ export default function ProductsSection({ products, loading }) {
     if (isAuthenticated) dispatch(getWishList(user.api_token));
   }, [dispatch, isAuthenticated, user]);
 
+  const PRODUCT_COUNT = 9;
+
   return (
     <>
       {tabViewPro && (
         <Drawer
-          anchor={"left"}
+          anchor={'left'}
           open={isFilterOpen}
-          onClose={toggleDrawer("left", false)}
-          onOpen={toggleDrawer("left", true)}
+          onClose={toggleDrawer('left', false)}
+          onOpen={toggleDrawer('left', true)}
           transitionDuration={600}
         >
           <Filter />
@@ -91,8 +93,8 @@ export default function ProductsSection({ products, loading }) {
 
       <Grid
         container
-        style={{ width: "100%", margin: 0 }}
-        justifyContent="flex-start"
+        style={{ width: '100%', margin: 0 }}
+        justifyContent='flex-start'
         spacing={mobileView ? 1 : 3}
       >
         <Grid
@@ -100,15 +102,15 @@ export default function ProductsSection({ products, loading }) {
           xs={12}
           md={12}
           style={{
-            display: "flex",
-            justifyContent: tabViewPro ? "space-between" : "flex-end",
-            alignItems: "center",
-            marginBottom: "1rem",
+            display: 'flex',
+            justifyContent: tabViewPro ? 'space-between' : 'flex-end',
+            alignItems: 'center',
+            marginBottom: '1rem',
           }}
         >
           {tabViewPro && (
             <div className={styles.filterDiv}>
-              <ButtonGroup variant="contained" color="default" aria-label="">
+              <ButtonGroup variant='contained' color='default' aria-label=''>
                 <Button
                   onClick={() => setFilterOpen(true)}
                   className={cx(styles.btn, styles.filterBtn)}
@@ -123,57 +125,56 @@ export default function ProductsSection({ products, loading }) {
           )}
 
           <FormControl
-            size="small"
-            variant="outlined"
-            style={{ minWidth: "130px" }}
+            size='small'
+            variant='outlined'
+            style={{ minWidth: '130px' }}
           >
             <InputLabel
-              color={"secondary"}
-              style={{ fontWeight: "700", color: "#6A5B40", fontSize: "16px" }}
+              color={'secondary'}
+              style={{ fontWeight: '700', color: '#6A5B40', fontSize: '16px' }}
             >
               Sort by
             </InputLabel>
             <Select
               value={sortBy}
-              onChange={(e) => handleSort(e)}
-              label="Sort by"
+              onChange={e => handleSort(e)}
+              label='Sort by'
             >
               <MenuItem
-                value={"relavence"}
-                style={{ fontSize: mobileView && "15px" }}
+                value={'relavence'}
+                style={{ fontSize: mobileView && '15px' }}
               >
                 Relavence
               </MenuItem>
               <MenuItem
-                value={"lowToHigh"}
-                style={{ fontSize: mobileView && "15px" }}
+                value={'lowToHigh'}
+                style={{ fontSize: mobileView && '15px' }}
               >
                 Low to High
               </MenuItem>
               <MenuItem
-                value={"highToLow"}
-                style={{ fontSize: mobileView && "15px" }}
+                value={'highToLow'}
+                style={{ fontSize: mobileView && '15px' }}
               >
                 High to Low
               </MenuItem>
             </Select>
           </FormControl>
         </Grid>
-        {loading ? (
-          <div style={{ margin: "auto" }}>
-            <Loader height={"200px"} />
-          </div>
-        ) : (
-          <div className={styles.productsGrid}>
-            {products?.map((value, index) => {
-              return (
-                <>
-                  <ProductCard key={index.id?.toString()} product={value} />
-                </>
-              );
-            })}
-          </div>
-        )}
+
+        <div className={styles.productsGrid}>
+          {loading
+            ? [...Array(PRODUCT_COUNT)].map(item => {
+                return <ProductLoader width='100%' height='400px' />;
+              })
+            : products?.map((value, index) => {
+                return (
+                  <>
+                    <ProductCard key={index.id?.toString()} product={value} />
+                  </>
+                );
+              })}
+        </div>
 
         {/* {tabView && (
           <>
--- src/Pages/Designers-Product-Page/Components/product-card/card.jsx
+++ src/Pages/Designers-Product-Page/Components/product-card/card.jsx
@@ -1,52 +1,53 @@
-import React, { useState, useEffect } from "react";
-import { IconButton } from "@material-ui/core";
-import styles from "./card.module.scss";
-import { Link } from "react-router-dom";
-import parse from "html-react-parser";
+import React, { useState, useEffect } from 'react';
+import { IconButton } from '@material-ui/core';
+import styles from './card.module.scss';
+import { Link } from 'react-router-dom';
+import parse from 'html-react-parser';
 //icon
 
-import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import common_axios from "../../../../utils/axios.config";
-import { useDispatch, useSelector } from "react-redux";
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import common_axios from '../../../../utils/axios.config';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   addToWishlist,
   clearUpdateWishlist,
   removeFromWishlist,
-} from "../../../../Redux/actions/wishlist";
-import { getWishList } from "../../../../Redux/actions/wishlist";
+} from '../../../../Redux/actions/wishlist';
+import { getWishList } from '../../../../Redux/actions/wishlist';
 
-import { LazyLoadingImg } from "../../../../utils/LazyLoading";
-import { CLEAR_WISHLIST_UPDATE } from "../../../../Redux/actions/types";
+import { LazyLoadingImg } from '../../../../utils/LazyLoading';
+import { CLEAR_WISHLIST_UPDATE } from '../../../../Redux/actions/types';
 
 export default function ProductCard(props) {
+  console.log('🚀 ~ file: card.jsx ~ line 23 ~ ProductCard ~ props', props);
   const dispatch = useDispatch();
   const [isAddToWishList, setAddToWishList] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [product, setProduct] = useState({});
 
-  const { user, isAuthenticated } = useSelector((state) => state.root.auth);
-  const { added, removed } = useSelector((state) => state.root.updateWishlist);
-  const { list } = useSelector((state) => state.root.wishlist);
+  const { user, isAuthenticated } = useSelector(state => state.root.auth);
+  const { added, removed } = useSelector(state => state.root.updateWishlist);
+  const { list } = useSelector(state => state.root.wishlist);
   // console.log(props.product);
 
-  const setValue = async (product) => {
+  const setValue = async product => {
     await setIsLoading(true);
     await setProduct(product);
     await setIsLoading(false);
   };
 
-  const add_to_wishlist = (prod) => {
+  const add_to_wishlist = prod => {
     if (!user?.api_token)
-      return alert("Login first to add the item to wishlist");
+      return alert('Login first to add the item to wishlist');
     dispatch(addToWishlist(prod.slug, user.api_token));
     setAddToWishList(true);
     dispatch(getWishList(user.api_token));
   };
 
-  const remove_from_wishlist = (prod) => {
+  const remove_from_wishlist = prod => {
     if (!user?.api_token)
-      return alert("Login first to add the item to wishlist");
+      return alert('Login first to add the item to wishlist');
     const id = prod.id;
     dispatch(removeFromWishlist(id, user.api_token));
     dispatch(getWishList(user.api_token));
@@ -55,7 +56,7 @@ export default function ProductCard(props) {
 
   useEffect(() => {
     if (isAuthenticated && list.length > 0) {
-      const item = list.filter((data) => data.product_id === props.product.id);
+      const item = list.filter(data => data.product_id === props.product.id);
 
       if (item[0]?.product_id) setAddToWishList(true);
     } else setAddToWishList(false);
@@ -77,21 +78,22 @@ export default function ProductCard(props) {
     <div key={props.key} className={styles.container}>
       <div className={styles.imgContainer}>
         <Link to={{ pathname: `/product-description/${product.slug}` }}>
-          <LazyLoadingImg image={product.feature_image} />
+          {/* <LazyLoadingImg image={product.feature_image} /> */}
+          <img src={product.feature_image} alt='' />
         </Link>
         {isAddToWishList ? (
           <IconButton
-            aria-label="product"
+            aria-label='product'
             onClick={() => {
               remove_from_wishlist(product);
             }}
             className={styles.icons}
           >
-            <FavoriteIcon style={{ color: "red" }} />
+            <FavoriteIcon style={{ color: 'red' }} />
           </IconButton>
         ) : (
           <IconButton
-            aria-label="product"
+            aria-label='product'
             onClick={() => add_to_wishlist(product)}
             className={styles.icons}
           >
@@ -100,11 +102,11 @@ export default function ProductCard(props) {
         )}
       </div>
       <div className={styles.productDetails}>
-        <Link to={{ pathname: `/product-description/${product.slug}` }}>
+        <Link to={{ pathname: `/product-description/${product?.slug}` }}>
           <span className={styles.productName}>{product?.brand}</span>
         </Link>
         <span className={styles.productDesc}>
-          {parse(product.description ? product?.title : "")}
+          {parse(product.description ? product?.title : '')}
         </span>
 
         <p className={styles.productPrice}>

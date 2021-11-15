d87975176823b3dc6d235dcbf1a6ca1c8e9aac83
Yogesh Kumar <yogesh.kumar.rr5@gmail.com>
2021-11-15 18:31:15 +0530

    Order Page UI

--- src/utils/Custom Divider/divider.jsx
+++ src/utils/Custom Divider/divider.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React from 'react';
 
 export default function CustomDivider(props) {
   return (
     <div
-      style={{ height: "1px", background: "#9D8E73", flex: 1, ...props.style }}
+      style={{
+        height: '1px',
+        background: `${props.customBg || '#9D8E73'}`,
+        flex: 1,
+        ...props.style,
+      }}
     ></div>
   );
 }

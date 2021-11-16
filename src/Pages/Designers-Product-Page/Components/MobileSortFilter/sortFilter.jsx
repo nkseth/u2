2ec86818b551305408d9b379ee9a8c67cb3dd581
Yogesh Kumar <yogesh.kumar.rr5@gmail.com>
2021-11-16 14:46:29 +0530

    Fixed some issues in Product Listing

--- src/Pages/Designers-Product-Page/Components/MobileSortFilter/sortFilter.jsx
+++ src/Pages/Designers-Product-Page/Components/MobileSortFilter/sortFilter.jsx
@@ -1,147 +1,185 @@
-import { Button, useMediaQuery, FormControl, RadioGroup, FormControlLabel, Radio, Drawer, withStyles } from '@material-ui/core';
+import {
+  Button,
+  useMediaQuery,
+  FormControl,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  Drawer,
+  withStyles,
+} from '@material-ui/core';
 import React, { useState, useEffect } from 'react';
-import sort from '../../../../Images/image/sort.png'
-import filter from '../../../../Images/image/filtering.png'
-import styles from './sortFilter.module.scss'
+import sort from '../../../../Images/image/sort.png';
+import filter from '../../../../Images/image/filtering.png';
+import styles from './sortFilter.module.scss';
 import CustomDivider from '../../../../utils/Custom Divider/divider';
 import Close from '../../../../Images/image/closing.png';
 import FilterMobile from '../Sections/FilterMobile/filter';
 
-
 export default function SortFilter() {
-    const mobileView = useMediaQuery("(max-width:550px)");
-    const [isSortOpen, setSortOpen] = useState(false);
-    const [isFilterOpen, setFilterOpen] = useState(false);
+  const mobileView = useMediaQuery('(max-width:550px)');
+  const [isSortOpen, setSortOpen] = useState(false);
+  const [isFilterOpen, setFilterOpen] = useState(false);
 
+  const CustomRadio = withStyles({
+    root: {
+      color: '#9D9D9D',
+      '&$checked': {
+        color: '#857250',
+      },
+    },
+    checked: {},
+  })(props => <Radio color='default' {...props} />);
 
-    const CustomRadio = withStyles({
-        root: {
-            color: '#9D9D9D',
-            '&$checked': {
-                color: '#857250',
-            },
-        },
-        checked: {},
-    })(props => <Radio color='default' {...props} />);
+  const toggleSortDrawer = (anchor, open) => event => {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return;
+    }
 
-    const toggleSortDrawer = (anchor, open) => (event) => {
-        if (
-            event &&
-            event.type === "keydown" &&
-            (event.key === "Tab" || event.key === "Shift")
-        ) {
-            return;
-        }
+    setSortOpen(open);
+  };
 
-        setSortOpen(open);
-    };
+  const toggleFilterDrawer = (anchor, open) => event => {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return;
+    }
 
-    const toggleFilterDrawer = (anchor, open) => (event) => {
-        if (
-            event &&
-            event.type === "keydown" &&
-            (event.key === "Tab" || event.key === "Shift")
-        ) {
-            return;
-        }
+    setFilterOpen(open);
+  };
 
-        setFilterOpen(open);
-    };
+  return (
+    <div className={styles.container}>
+      {mobileView && (
+        <div>
+          <Drawer
+            anchor={'bottom'}
+            open={isSortOpen}
+            onOpen={toggleSortDrawer('bottom', true)}
+            transitionDuration={600}
+          >
+            <div className={styles.sortsection}>
+              <div>
+                <h4 className={styles.label}>Sort</h4>
+                <img src={Close} onClick={() => setSortOpen(false)} />
+              </div>
 
-    return (
-        <div className={styles.container}>
-            {mobileView && (
-                <div>
-                    <Drawer
-                        anchor={"bottom"}
-                        open={isSortOpen}
-                        onOpen={toggleSortDrawer("bottom", true)}
-                        transitionDuration={600}
-                    >
-                        <div className={styles.sortsection}>
-                            <div>
-                                <h4 className={styles.label}>Sort</h4>
-                                <img src={Close} onClick={() => setSortOpen(false)}/>
-                            </div>
+              <FormControl style={{ width: '100%' }} component='fieldset'>
+                <RadioGroup
+                  defaultValue='Best Match'
+                  name='radio-buttons-group'
+                >
+                  <FormControlLabel
+                    value='Best Match'
+                    control={<CustomRadio />}
+                    label={
+                      <span className={styles.radioLabel}>Best Match </span>
+                    }
+                  />
+                  <hr className={styles.divider} />
+                  <FormControlLabel
+                    value='Price: High to Low'
+                    control={<CustomRadio />}
+                    label={
+                      <span className={styles.radioLabel}>
+                        Price: High to Low
+                      </span>
+                    }
+                  />
+                  <hr className={styles.divider} />
+                  <FormControlLabel
+                    value='Price: Low to High'
+                    control={<CustomRadio />}
+                    label={
+                      <span className={styles.radioLabel}>
+                        {' '}
+                        Price: Low to High
+                      </span>
+                    }
+                  />
+                  <hr className={styles.divider} />
+                  <FormControlLabel
+                    value='New Arrivals'
+                    control={<CustomRadio />}
+                    label={
+                      <span className={styles.radioLabel}>New Arrivals</span>
+                    }
+                  />
+                  <hr className={styles.divider} />
+                  <FormControlLabel
+                    value='Most liked'
+                    control={<CustomRadio />}
+                    label={
+                      <span className={styles.radioLabel}>Most liked</span>
+                    }
+                  />
+                  <hr className={styles.divider} />
+                  <FormControlLabel
+                    value='Discount: High to Low'
+                    control={<CustomRadio />}
+                    label={
+                      <span className={styles.radioLabel}>
+                        Discount: High to Low
+                      </span>
+                    }
+                  />
+                </RadioGroup>
+              </FormControl>
+            </div>
+          </Drawer>
 
-                            <FormControl style={{width:'100%'}} component="fieldset">
-                                <RadioGroup
-                                    defaultValue="Best Match"
-                                    name="radio-buttons-group"
-                                >
-                                    <FormControlLabel
-                                        value="Best Match"
-                                        control={<CustomRadio />}
-                                        label={<span className={styles.radioLabel}>Best Match </span>}
-                                    />
-                                    <hr className={styles.divider}/>
-                                    <FormControlLabel
-                                        value="Price: High to Low"
-                                        control={<CustomRadio />}
-                                        label={<span className={styles.radioLabel}>Price: High to Low</span>}
-                                    />
-                                    <hr className={styles.divider}/>
-                                    <FormControlLabel
-                                        value="Price: Low to High"
-                                        control={<CustomRadio />}
-                                        label={<span className={styles.radioLabel}> Price: Low to High</span>}
-                                    />
-                                    <hr className={styles.divider}/>
-                                    <FormControlLabel
-                                        value="New Arrivals"
-                                        control={<CustomRadio />}
-                                        label={<span className={styles.radioLabel}>New Arrivals</span>}
-                                    />
-                                    <hr className={styles.divider}/>
-                                    <FormControlLabel
-                                        value="Most liked"
-                                        control={<CustomRadio />}
-                                        label={<span className={styles.radioLabel}>Most liked</span>}
-                                    />
-                                    <hr className={styles.divider}/>
-                                    <FormControlLabel
-                                        value="Discount: High to Low"
-                                        control={<CustomRadio />}
-                                        label={<span className={styles.radioLabel}>Discount: High to Low</span>}
-                                    />
+          <Drawer
+            anchor={'bottom'}
+            open={isFilterOpen}
+            onClose={toggleFilterDrawer('bottom', false)}
+            onOpen={toggleFilterDrawer('bottom', true)}
+            transitionDuration={600}
+          >
+            <FilterMobile filter={isFilterOpen} toggle={toggleFilterDrawer} />
+          </Drawer>
 
-                                </RadioGroup>
-                            </FormControl>
-                        </div>
+          <div style={{ display: 'flex' }}>
+            <div
+              style={{
+                width: '50%',
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              <Button
+                className={styles.filter}
+                onClick={() => setSortOpen(true)}
+              >
+                <img src={sort} style={{ marginRight: '12px' }} />
+                Sort
+              </Button>
+            </div>
 
-                    </Drawer>
-
-                    <Drawer
-                        anchor={"bottom"}
-                        open={isFilterOpen}
-                        onClose={toggleFilterDrawer("bottom", false)}
-                        onOpen={toggleFilterDrawer("bottom", true)}
-                        transitionDuration={600}
-                    >
-                        <FilterMobile  filter={isFilterOpen}/>
-                    </Drawer>
-
-                    <div style={{ display: "flex" }}>
-                        <div style={{ width: '50%', display: 'flex', justifyContent: 'center' }}>
-                            <Button className={styles.filter}
-                                onClick={() => setSortOpen(true)}
-                            >
-                                <img src={sort} style={{ marginRight: '12px' }} />
-                                Sort</Button>
-                        </div>
-
-                        <div style={{ width: '50%', display: 'flex', justifyContent: 'center' }}>
-                            <Button className={styles.filter}
-                                onClick={() => setFilterOpen(true)}
-                            >
-                                <img src={filter} style={{ marginRight: '12px' }} alt="" />
-                                Filter
-                            </Button>
-                        </div>
-
-                    </div>
-                </div>
-            )}
+            <div
+              style={{
+                width: '50%',
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              <Button
+                className={styles.filter}
+                onClick={() => setFilterOpen(true)}
+              >
+                <img src={filter} style={{ marginRight: '12px' }} alt='' />
+                Filter
+              </Button>
+            </div>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 }
